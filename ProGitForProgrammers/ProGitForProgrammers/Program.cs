25f5551fe324403b8bb95f36691dfc8ae5aeee88
horacekangholie <horacekangholie@gmail.com>
2024-11-23 17:12:11 +0800

    Add a line to show example for staging area - from Visual Studio

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -7,6 +7,8 @@ namespace ProGitForProgrammers
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("This line add to show the staging area");
         }
     }
 }
